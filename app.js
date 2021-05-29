5c7ac164b7363ce33edde0460c00463871e5b874
Shiv Pratap Singh Parmar <spsparmar1729@gmail.com>
2021-05-29 23:13:00 +0530

    Enabled cluster module

--- app.js
+++ app.js
@@ -12,19 +12,17 @@ app.get('/', (req, res) => {
     // some long
   }
   res.send(`Ok...${process.pid}`);
-  // cluster.worker.kill();
+  cluster.worker.kill();
 });
 
-// if (cluster.isMaster) {
-//   for (let i = 0; i < numCpu; i++) {
-//     cluster.fork();
-//   }
-//   cluster.on('exit', (worker, code, signal) => {
-//     console.log(`worker: ${worker.process.pid} died.`);
-//     cluster.fork();
-//   })
-// } else {
-//   app.listen(3000, () => console.log(`Server ${process.pid} running ✅ on port 3000...`));
-// }
-
-app.listen(3000, () => console.log(`Server ${process.pid} running ✅ on port 3000...`));
+if (cluster.isMaster) {
+  for (let i = 0; i < numCpu; i++) {
+    cluster.fork();
+  }
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker: ${worker.process.pid} died.`);
+    cluster.fork();
+  })
+} else {
+  app.listen(3000, () => console.log(`Server ${process.pid} running ✅ on port 3000...`));
+}
